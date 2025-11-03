66060ff1f0981d6c0aae64813b2f22eb590e3061
berneeyeh <berneeyeh@gmail.com>
2025-11-03 20:07:33 +0800

    Create ex3-2.py
    #字串 (String)
    name = "20"
    message = '歡迎使用Python'
    
    # 整數 (Integer)
    age = 25
    score = 100
    
    # 浮點數(Floot)
    height = 175.6
    price = 99.99
    
    #布林值(BooLean)
    is_student = True
    has_license = False
    
    # 檢查變數類型
    print(name)
    print(type(name))  # <class 'str'>
    print(type(age))  # <class 'int'>
    print(type(height))  # <class 'float'>
    print(type(is_student))  # <class 'bool'>

---
+++ ex3-2.py
@@ -0,0 +1,22 @@
+#字串 (String)
+name = "20"
+message = '歡迎使用Python'
+
+# 整數 (Integer)
+age = 25
+score = 100
+
+# 浮點數(Floot)
+height = 175.6
+price = 99.99
+
+#布林值(BooLean)
+is_student = True
+has_license = False
+
+# 檢查變數類型
+print(name)
+print(type(name))  # <class 'str'>
+print(type(age))  # <class 'int'>
+print(type(height))  # <class 'float'>
+print(type(is_student))  # <class 'bool'>
